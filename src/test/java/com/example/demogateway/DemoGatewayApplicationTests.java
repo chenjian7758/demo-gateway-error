a634e097964d227a2f4f43811663064ef9e0c348
Rubén García Pariente <ruben.garciapariente@gruposantander.es>
2019-07-24 19:19:56 +0200

    Only GW

--- src/test/java/com/example/demogateway/DemoGatewayApplicationTests.java
+++ src/test/java/com/example/demogateway/DemoGatewayApplicationTests.java
@@ -2,61 +2,94 @@ package com.example.demogateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.multipart.MultipartHttpMessageReader;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.MultiValueMap;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.ProxyProvider;
+import reactor.netty.tcp.TcpClient;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = DEFINED_PORT)
-@Import(TestController.class)
 public class DemoGatewayApplicationTests {
 
-	@LocalServerPort
-	protected int port = 0;
+    //@LocalServerPort
+    protected int port = 8080;
 
-	@Test
-	public void contextLoads() {
+    @Test
+    public void testGW() {
 
-		String baseUri = "http://localhost:" + port;
+        /*
+        TcpClient tcpClient = TcpClient.create().proxy(options -> options.type(ProxyProvider.Proxy.HTTP).host("localhost").port(8888));
+        HttpClient client = HttpClient.from(tcpClient);
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(client);
+        */
 
-		WebTestClient testClient = WebTestClient.bindToServer()
-				.baseUrl(baseUri)
-				.build();
+        String baseUri = "http://localhost:8080";
 
-		testClient.post()
-				.uri("/gw/test/post")
-				.contentType(MediaType.MULTIPART_FORM_DATA)
-				.syncBody(generateBody())
-				.exchange()
-				.expectStatus()
-				.isOk()
-				.expectBody(String.class)
-				.consumeWith(result -> {
-					assertTrue(result.getResponseBody().contains("data"));
-					assertTrue(result.getResponseBody().contains("base64"));
-				});
+        WebTestClient testClient = WebTestClient.bindToServer()
+                .baseUrl(baseUri)
+                .build();
 
-	}
+        testClient.post()
+                .uri("/gw/test/post")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .syncBody(generateBody())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .consumeWith(result -> {
+                    assertTrue(result.getResponseBody().contains("data"));
+                    assertTrue(result.getResponseBody().contains("base64"));
+                });
 
-	private MultiValueMap<String, HttpEntity<?>> generateBody() {
-		ClassPathResource file = new ClassPathResource("/test.txt", MultipartHttpMessageReader.class);
-		MultipartBodyBuilder builder = new MultipartBodyBuilder();
-		builder.part("file", file);
-		return builder.build();
-	}
+    }
+
+    @Test
+    public void testMicro() {
+
+        /*
+        TcpClient tcpClient = TcpClient.create().proxy(options -> options.type(ProxyProvider.Proxy.HTTP).host("localhost").port(8888));
+        HttpClient client = HttpClient.from(tcpClient);
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(client);
+         */
+
+        String baseUri = "http://localhost:8082";
+
+        WebTestClient testClient = WebTestClient.bindToServer()
+                .baseUrl(baseUri)
+                .build();
+
+        testClient.post()
+                .uri("/test/post")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .syncBody(generateBody())
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .consumeWith(result -> {
+                    assertTrue(result.getResponseBody().contains("data"));
+                    assertTrue(result.getResponseBody().contains("base64"));
+                });
+
+    }
+
+
+    private MultiValueMap<String, HttpEntity<?>> generateBody() {
+        ClassPathResource file = new ClassPathResource("/test.txt", MultipartHttpMessageReader.class);
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("file", file);
+        return builder.build();
+    }
 
 
 }
